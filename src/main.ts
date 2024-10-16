186b4fdb161e6695b2d6bc6e004ed4821d5a1e2a
Ana Osorio Mondragon <110679613+ana-om@users.noreply.github.com>
2024-10-15 22:17:06 -0500

    feat(*): add lazy loading and home component (#5)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+// import { enableProdMode } from '@angular/core';
+// import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+// if (environment.production) {
+//   enableProdMode();
+// }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, {
+    ngZoneEventCoalescing: true,
+  })
   .catch(err => console.error(err));
